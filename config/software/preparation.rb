f5a3731bdd14c54f855eda2c04aca5a7eacc05be
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-05-23 11:15:59 -0400

    Add explicit calls

--- config/software/preparation.rb
+++ config/software/preparation.rb
@@ -19,14 +19,12 @@ name 'preparation'
 description 'the steps required to prepare the build'
 default_version '1.0.0'
 
-sudome
-
 build do
   block do
     %w(embedded/lib embedded/bin bin).each do |dir|
       dir_fullpath = File.expand_path(File.join(install_dir, dir))
-      FileUtils.mkdir_p(dir_fullpath)
-      FileUtils.touch(File.join(dir_fullpath, '.gitkeep'))
+      sudome(FileUtils.mkdir_p(dir_fullpath))
+      sudome(FileUtils.touch(File.join(dir_fullpath, '.gitkeep')))
     end
   end
 end
