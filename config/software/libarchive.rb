feda710900a53d05fa1bdd6eb29f95bdd5d95d96
John Keiser <john@johnkeiser.com>
2016-08-04 12:30:39 -0700

    Update libarchive to use configure correctly on Windows

--- config/software/libarchive.rb
+++ config/software/libarchive.rb
@@ -35,7 +35,7 @@ build do
   env = with_standard_compiler_flags(with_embedded_path)
   update_config_guess(target: "build/autoconf/")
 
-  configure_args = [
+  configure_commands = [
     "--prefix=#{install_dir}/embedded",
     "--without-lzma",
     "--without-lzo2",
@@ -51,11 +51,11 @@ build do
     "--without-openssl",
   ]
 
-  if s390x?
-    configure_args << "--disable-xattr --disable-acl"
-  end
+  # Don't leave static libraries on windows.
+  configure_commands << "--disable-static" if windows?
+  configure_commands << "--disable-xattr --disable-acl" if s390x?
 
-  configure configure_args.join(" "), env: env
+  configure(*configure_commands, env: env)
 
   make "-j #{workers}", env: env
   make "-j #{workers} install", env: env
